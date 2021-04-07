2ee2e67dc282a4438fb18088d3ef521b54e57f3d
TechAle <alessandro.condello.email@gmail.com>
2021-04-07 11:25:43 +0200

    Added custom Height and Width

--- src/main/java/me/srgantmoomoo/postman/api/mixin/mixins/MixinChat.java
+++ src/main/java/me/srgantmoomoo/postman/api/mixin/mixins/MixinChat.java
@@ -6,7 +6,9 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GuiNewChat.class)
 public class MixinChat {
@@ -15,6 +17,18 @@ public class MixinChat {
     @Redirect(method={"drawChat"}, at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V"))
     private void drawRectHook(int left, int top, int right, int bottom, int color) {
         Gui.drawRect(left, top, right, bottom, ModuleManager.isModuleEnabled("CustomChat") ? CustomChat.backColorInt : color);
+    }
+
+    @Inject(method = {"getChatHeight"}, at = @At("HEAD"), cancellable = true)
+    public void getChatHeight(CallbackInfoReturnable<Integer> cir) {
+        if (CustomChat.maxHeightInt != -1)
+            cir.setReturnValue(CustomChat.maxHeightInt);
+    }
+
+    @Inject(method = {"getChatWidth"}, at = @At("HEAD"), cancellable = true)
+    public void getChatWidth(CallbackInfoReturnable<Integer> cir) {
+        if (CustomChat.maxWidthInt != -1)
+            cir.setReturnValue(CustomChat.maxWidthInt);
     }
 
 }
--- src/main/java/me/srgantmoomoo/postman/client/module/modules/render/CustomChat.java
+++ src/main/java/me/srgantmoomoo/postman/client/module/modules/render/CustomChat.java
@@ -4,6 +4,7 @@ import me.srgantmoomoo.postman.api.util.render.JColor;
 import me.srgantmoomoo.postman.client.module.Category;
 import me.srgantmoomoo.postman.client.module.Module;
 import me.srgantmoomoo.postman.client.setting.settings.ColorSetting;
+import me.srgantmoomoo.postman.client.setting.settings.NumberSetting;
 import net.minecraft.world.BossInfo;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import org.lwjgl.input.Keyboard;
@@ -14,18 +15,22 @@ public class CustomChat extends Module {
 
     // The back color is the same as default one (Color.decode(Integer.toString(2130706432)))
     public ColorSetting backColor = new ColorSetting("BackColor", this, new JColor(0, 0, 0));
-    // 2'130'706'432
-    // 2130706432
+    public NumberSetting maxHeight = new NumberSetting("Max Height", this, 200, -1, 1000, 1);
+    public NumberSetting maxWidth = new NumberSetting("Max Width", this, 200, -1, 1000, 1);
+
     public CustomChat() {
         super ("CustomChat", "Allow you to custom your chat", Keyboard.KEY_NONE, Category.RENDER);
-        this.settings.add(backColor);
+        this.addSettings(backColor, maxHeight, maxWidth);
     }
-    public static int backColorInt;
+    public static int backColorInt,
+                      maxHeightInt,
+                      maxWidthInt;
 
     @Override
     public void onUpdate() {
         backColorInt = backColor.getValue().getRGB();
-
+        maxHeightInt = (int) maxHeight.getValue();
+        maxWidthInt = (int) maxWidth.getValue();
     }
 
 
